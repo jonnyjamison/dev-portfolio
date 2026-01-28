b19b1791b35a38a0b9ad1df6ab0d79db471da4ef
jonnyjamison <jonny.jamison@outlook.com>
2026-01-28 21:19:55 +0000

    Removal of leading '/' from media in config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://jonnyjamison.github.io",
+  base: "/dev-portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
